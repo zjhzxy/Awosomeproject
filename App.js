a1c6c377dad5a4e82425340aa02754bfb363f4b3
react-native run-android <15158011164@163.com>
2018-05-12 21:40:58 +0800

    react-native run-android

--- App.js
+++ App.js
@@ -8,20 +8,33 @@ import React, { Component } from 'react';
 import {
   View,
   StyleSheet,
+  FlatList,
   } from 'react-native';
+import Item from './src/components/Item';
+import movies from './movies.json'
+
 const styles = StyleSheet.create({
-  root:{
-    backgroundColor: 'red',
-    width:50,
-    height:60,
-  }
+  row:{
+    paddingHorizontal:15,
+  },
 });
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.root} >
-
+      <View>
+        <FlatList
+          style={styles.row}
+          numColumns={3}
+          keyExtractor={item=>item.id}
+          data={movies.subjects}
+          renderItem={({item}) =>
+            <Item
+              title={item.title}
+              image={item.images.medium}
+              stars={item.rating.stars}
+            />}
+        />
       </View>
     );
   }
---
+++ src/components/Item.js
@@ -0,0 +1,115 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  Dimensions,
+} from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+const thirdWidth = width / 3;
+const imageWidth = thirdWidth-10*2;
+const imageHeight = imageWidth / 0.697;
+const styles = StyleSheet.create({
+  root:{
+    marginTop:20,
+    width:imageWidth,
+    //alignItems:'center',
+    marginRight:15,
+    //height:60,
+  },
+  image:{
+    width:imageWidth,
+    height:imageHeight,
+  },
+  title:{
+    fontSize:20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop:5,
+  },
+  starWrapper:{
+    flexDirection:'row',
+  },
+  stars:{
+    width:15,
+    height:15,
+  },
+});
+const renderStars = (stars) => {
+  if(stars === '00'){
+    return;
+  }
+  const total = 5;
+  let full, half, empty;
+  full = parseInt(stars[0]);
+  if(stars[1]==='5'){
+    full++;
+    half=0;
+    empty=total - full;
+  }else {
+    half = 1;
+    empty = total - full - half;
+  }
+  const results=[];
+  let i;
+  for(i=0; i<full; i++){
+    results.push(
+      <Image
+      style={styles.stars}
+      key={i}
+      source={require('../img/star-full.png')}
+    />
+    );
+  }
+  if(half){
+    results.push(
+      <Image
+        style={styles.stars}
+        key={i}
+        source={require('../img/star-half.png')}
+      />
+    );
+  }
+  for(let j=0; j<empty; j++){
+    results.push(
+      <Image
+        style={styles.stars}
+        key={i+j+1}
+        source={require('../img/star-empty.png')}
+      />
+    );
+  }
+
+  return (
+    <View style={styles.starWrapper}>
+      {results}
+    </View>
+  );
+}
+const Item = (props) => {
+    const {title, image, stars} = props;
+    return (
+      <View style={styles.root} >
+        <Image
+          source={{uri:image}}
+          style={styles.image}
+        />
+        <Text
+          numberOfLines = {1}
+          style={styles.title}
+        >
+          {title}
+        </Text>
+        {renderStars(stars)}
+      </View>
+    );
+  };
+export default Item
